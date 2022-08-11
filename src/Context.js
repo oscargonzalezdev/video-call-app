f58a3d01e923ec3869ecff1c22ff7cc9198da73b
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-08-11 18:17:43 +0200

    fix(socket.io): change server url

--- src/Context.js
+++ src/Context.js
@@ -4,10 +4,14 @@ import Peer from 'simple-peer'
 
 const SocketContext = createContext()
 // const socket = io('http://localhost:5001')
-// const socket = io('https://video-call-server-pi.vercel.app')
-const socket = io("wss://video-call-server-pi.vercel.app", {
+const socket = io("https://video-call-server-pi.vercel.app", {
   transports: ["websocket", "polling"] // use WebSocket first, if available
 });
+
+socket.on("connect_error", () => {
+    // revert to classic upgrade
+    socket.io.opts.transports = ["polling", "websocket"];
+  });
 
 const ContextProvider = ({ children }) => {
     const [stream, setStream] = useState(null)
