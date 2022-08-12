f6901feb8e811c41ea077754623c07b342a1f529
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-08-12 08:27:52 +0200

    fix(socket.io): change socket connection options

--- src/Context.js
+++ src/Context.js
@@ -5,6 +5,7 @@ import Peer from 'simple-peer'
 const SocketContext = createContext()
 // const socket = io("http://localhost:5001"
 const socket = io('https://video-call-server-pi.vercel.app', {
+    secure: true,
     forceNew: true,
     transports: ["polling"]
 });
