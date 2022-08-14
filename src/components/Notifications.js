5a5800c93f3097c500a071f851d59e477938b90e
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-08-14 19:34:43 +0200

    refactor(Notifications): use modal components to render Notifications

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,17 +1,48 @@
-import { useContext } from "react"
+
+import { useContext, useState } from "react"
 import { SocketContext } from "../Context"
+import { FcVideoCall, FcCancel } from 'react-icons/fc'
+import BeatLoader from "react-spinners/BeatLoader"
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalFooter,
+  ModalBody,
+} from '@chakra-ui/react'
 
 const Notifications = () => {
-  const { answerCall, call, callAccepted } = useContext(SocketContext)
+  const { answerCall, call, setCall, callAccepted, setCallAccepted, callEnded, userVideo } = useContext(SocketContext)
+  const [openModal, setOpenModal] = useState(true)
+
+  const aceptCall = () => {
+    answerCall()
+    setOpenModal(false)
+  }
 
   return (
     <>
-    { call.isReceivedCall && !callAccepted && (
-        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-          <h1>{call.name} is calling:</h1>
-          <button onClick={answerCall}>Answer</button>
+      {call.isReceivedCall && (
+        <div>
+          <Modal isOpen={openModal} isCentered={true}>
+            <ModalOverlay />
+            <ModalContent w='100%' p={10}>
+              <ModalBody>
+                <h1 className="text-center"><strong>{call.name} is calling <span><BeatLoader size={7} /></span></strong></h1>
+              </ModalBody>
+              <ModalFooter>
+                <Button marginRight={10} colorScheme='teal' variant='link' onClick={() => aceptCall()}><FcVideoCall size='50px' /></Button>
+                <Button colorScheme='teal' variant='link' onClick={() => setCall(false)}><FcCancel size='40px' /></Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </div>
-      )}
+      )
+      }
+
+
+
     </>
   )
 }
