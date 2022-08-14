81ed17327e6d239174ff63cac44267cc554f81d5
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-08-14 19:32:22 +0200

    refactor(Options): disable props children and use chackra components to style ui

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,34 +1,44 @@
+import { AspectRatio, Box, Input, Button, Spinner } from '@chakra-ui/react'
 import { useContext, useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { SocketContext } from '../Context'
+import { MdVideoCall } from 'react-icons/md'
 
 const Options = ({ children }) => {
-  const { me, callAccepted, callEnded, name, setName, leaveCall, callUser } = useContext(SocketContext)
-  const [idToCall, setIdToCall] = useState ('')
+  const { me, call, callAccepted, callEnded, name, setName, leaveCall, callUser } = useContext(SocketContext)
+  const [idToCall, setIdToCall] = useState('')
   const [isCopied, setIsCopied] = useState(false)
+  const [isClicked, setIsClicked] = useState(false)
 
+  if (isCopied) {
+    setTimeout(() => {
+      setIsCopied(false)
+    }, 3000)
+  }
+  if (isClicked) {
+    setTimeout(() => {
+      setIsClicked(false)
+    }, 5000)
+  }
+  console.log('call', call)
   return (
-    <div>
-    <h1>Options</h1>
-    <div>
-      <h2>Account Info</h2>
-      <input type="text" placeholder='Your Name' value={name} onChange={(e)=> {setName(e.target.value)}} />
-      <CopyToClipboard text={me} onCopy={() => setIsCopied(true)}>
-        { !isCopied ? <button>Copy your ID</button> : <button>Copied!</button> }
-      </CopyToClipboard>
-    </div>
-    <div>
-      <h2>Make a call</h2>
-      <input type="text" placeholder='ID to Call' value={idToCall} onChange={(e) => {setIdToCall(e.target.value)}} />
-      { callAccepted && !callEnded ? (
-          <button onClick={leaveCall}>Hang Up</button>
-        ) : (
-          <button onClick={()=> callUser(idToCall)}>Call</button>
-      )}
-      
-    </div>
-    <span>{ children }</span>
-    </div>
+    <Box boxShadow='md' rounded='lg' borderColor='#38B2AC80' borderWidth='2px' borderRadius='20px' overflow='hidden'>
+      <AspectRatio w='lg' ratio={4 / 3}>
+      <div className='flex-column'>
+        <h1 className='text-center'><strong>Call a friend!</strong></h1>
+        <div>
+          <div className='call-options'>
+            <Input required variant='filled' placeholder='Call ID' value={idToCall} onChange={(e) => { setIdToCall(e.target.value) }} />
+            <Button isLoading={isClicked} variant='none' border='0' type='submit' onClick={() => { callUser(idToCall); setIsClicked(true) }}><MdVideoCall color='green' size={90} /></Button>
+          </div>
+          <br />
+          <CopyToClipboard text={me} onCopy={() => setIsCopied(true)}>
+            <Button width='93%' colorScheme='teal' variant='outline' >{isCopied ? 'Copied!' : 'Copy my Call ID'}</Button>
+          </CopyToClipboard>
+        </div>
+        </div>
+      </AspectRatio>
+    </Box>
   )
 }
 export default Options
