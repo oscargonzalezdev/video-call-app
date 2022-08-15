f88ee370edc50a0dccb4f3ed76922a8570e29376
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-08-15 18:00:53 +0200

    refactor (Options): update class names and replace Button for IconButton component

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,11 @@
-import { AspectRatio, Box, Input, Button, Spinner } from '@chakra-ui/react'
+import { AspectRatio, Box, Input, Button, IconButton } from '@chakra-ui/react'
 import { useContext, useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { SocketContext } from '../Context'
 import { MdVideoCall } from 'react-icons/md'
 
 const Options = ({ children }) => {
-  const { me, call, callAccepted, callEnded, name, setName, leaveCall, callUser } = useContext(SocketContext)
+  const { me, callUser } = useContext(SocketContext)
   const [idToCall, setIdToCall] = useState('')
   const [isCopied, setIsCopied] = useState(false)
   const [isClicked, setIsClicked] = useState(false)
@@ -20,22 +20,23 @@ const Options = ({ children }) => {
       setIsClicked(false)
     }, 5000)
   }
-  console.log('call', call)
+
   return (
-    <Box boxShadow='md' rounded='lg' borderColor='#38B2AC80' borderWidth='2px' borderRadius='20px' overflow='hidden'>
+    <Box boxShadow='md' bg='rgba(222, 222, 222, 0.7)' rounded='lg' borderColor='#38B2AC80' borderWidth='2px' borderRadius='20px' overflow='hidden'>
       <AspectRatio w='lg' ratio={4 / 3}>
-      <div className='flex-column'>
-        <h1 className='text-center'><strong>Call a friend!</strong></h1>
-        <div>
-          <div className='call-options'>
-            <Input required variant='filled' placeholder='Call ID' value={idToCall} onChange={(e) => { setIdToCall(e.target.value) }} />
-            <Button isLoading={isClicked} variant='none' border='0' type='submit' onClick={() => { callUser(idToCall); setIsClicked(true) }}><MdVideoCall color='green' size={90} /></Button>
+        <div className='flex-column'>
+          <h1 className='text-center'><strong>Call a friend!</strong></h1>
+          <div>
+            <div className='flex-row call-options'>
+                <Input required variant='filled' placeholder='Call ID' value={idToCall} onChange={(e) => { setIdToCall(e.target.value) }} />
+                <IconButton isLoading={isClicked} disabled={idToCall.length < 7} onClick={()=>{callUser(idToCall); setIsClicked(true)}} variant='none' aria-label='Call' icon={<MdVideoCall color='green' size='80px'/>} />
+            </div>
+            <div>
+              <CopyToClipboard text={me} onCopy={() => setIsCopied(true)}>
+                <Button w='98%' colorScheme='teal' variant='outline' >{isCopied ? 'Copied!' : 'Copy my Call ID'}</Button>
+              </CopyToClipboard>
+            </div>
           </div>
-          <br />
-          <CopyToClipboard text={me} onCopy={() => setIsCopied(true)}>
-            <Button width='93%' colorScheme='teal' variant='outline' >{isCopied ? 'Copied!' : 'Copy my Call ID'}</Button>
-          </CopyToClipboard>
-        </div>
         </div>
       </AspectRatio>
     </Box>
