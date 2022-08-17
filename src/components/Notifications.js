cec6e6441cc7729244ac697df41176ad219adc02
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-08-17 13:32:13 +0200

    feat(notifications): add ring tone for incoming callss

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -13,13 +13,20 @@ import {
 } from '@chakra-ui/react'
 
 const Notifications = () => {
-  const { answerCall, call, setCall } = useContext(SocketContext)
+  const { answerCall, call, setCall, callAccepted } = useContext(SocketContext)
   const [openModal, setOpenModal] = useState(true)
+  const callSound = new Audio ('./call.mp3');
 
   const aceptCall = () => {
     answerCall()
     setOpenModal(false)
   }
+
+  const cancelCall = () => {
+    setCall(false)
+  }
+
+  call.isReceivedCall && !callAccepted && callSound.play()
 
   return (
     <>
@@ -27,13 +34,13 @@ const Notifications = () => {
         <div>
           <Modal isOpen={openModal} isCentered={true}>
             <ModalOverlay />
-            <ModalContent w='100%' p={10}>
+            <ModalContent borderRadius='20px' bg='#eaeaea' w='100%' p={10}>
               <ModalBody>
                 <h1 className="text-center"><strong>{call.name} is calling <span><BeatLoader size={7} /></span></strong></h1>
               </ModalBody>
               <ModalFooter>
                 <Button marginRight={10} colorScheme='teal' variant='link' onClick={() => aceptCall()}><FcVideoCall size='50px' /></Button>
-                <Button colorScheme='teal' variant='link' onClick={() => setCall(false)}><FcCancel size='40px' /></Button>
+                <Button colorScheme='teal' variant='link' onClick={() => cancelCall()}><FcCancel size='40px' /></Button>
               </ModalFooter>
             </ModalContent>
           </Modal>
